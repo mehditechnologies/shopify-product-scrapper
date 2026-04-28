f0741d8ca08fe66482170e16d18c8c7a343a3817
Fahad Hussain <fahadhussainf786@yahoo.com>
2026-04-28 17:14:01 +0500

    push

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { createClient } from "@/utils/supabase/client"
 
-// reset password function
+// reset-password function
 export default function ResetPassword() {
   const { theme } = useTheme()
   const router = useRouter()
